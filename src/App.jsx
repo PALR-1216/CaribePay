28d94a735dea88bf9f9c33c26a1ba9e863ae88fd
Pedro Lorenzo Rosario <senpai@Pedros-MacBook-Pro.local>
2024-12-13 08:08:16 -0400

    added the login auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 // src/App.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './Components/LoginView/LoginView';
 import DashBoard from './Components/Dashboard/DashBoardView';
 import TransactionView from './Components/Transaction/TransactionView';
 import SignUpView from './Components/SignUp/SignUpView';
+import authService from './Services/AuthService';
 
 function App() {
+  const navigator = 
+  useEffect(() => {
+    authService.verfiyAuth().then((response) => {
+      if (!response.success) {
+        navigator.navigate('/login');
+      } else {
+        navigator.navigate('/dashboard');
+      }
+    });
+
+  }, []);
   return (
     <div>
       {/* Navigation component will go here */}
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login"  element={<Login />} />
         <Route path="/dashboard" element={<DashBoard />} />
         <Route path="/transactions" element={<TransactionView />} />
         <Route path="/signUp" element={<SignUpView />} />
---
+++ src/Components/Dashboard/DashBoardView.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './DashBoard.css';
+
+export default function DashBoardView() {
+    return (
+        <div>
+        <h1 className='h1'>Dashboard</h1>
+        </div>
+    );
+}
